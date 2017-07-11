7251a6b59a0df981c8b699f9c632f5b91beda181
root <root@kikobr821.mylabserver.com>
2017-07-11 14:50:01 +0000

    Commit 170711145001 for folder ssh

---
+++ manifests/service.pp
@@ -0,0 +1,13 @@
+class ssh::service (
+  String $service_name    = $::ssh::service_name,
+  String $service_ensure  = $::ssh::service_ensure,
+  Boolean $service_enable = $::ssh::service_enable
+) {
+  service {  'ssh-service':
+    name       => $service_name,
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+}
